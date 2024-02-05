84b3c70e0b39980da1bf550089f38ae69121e5a4
Mr_Kel <papaez08@gmail.com>
2024-02-05 22:45:28 +1000

    Och laganni kod

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import os
 import sys
 
 import requests
+from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 SCREEN_SIZE = [600, 450]
-x, y = map(float, input().split())
-scale = float(input())
+x, y = map(float, '43.125505, 131.888700'.split(', '))
+scale = 1
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi('design.ui', self)
+
+        self.pushButton.clicked.connect(self.plus)
+        self.pushButton_2.clicked.connect(self.minus)
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&z={scale}&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={y},{x}&z={scale}&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -26,29 +31,41 @@ class Example(QWidget):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
     def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setGeometry(100, 100, 600, 550)
         self.setWindowTitle('Отображение карты')
 
-        ## Изображение
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
-        self.image.move(0, 0)
+        self.image.move(0, 100)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
+    def plus(self):
+        global scale
+        if scale != 21:
+            scale += 1
+            self.getImage()
+            self.pixmap = QPixmap(self.map_file)
+            self.image.setPixmap(self.pixmap)
 
+    def minus(self):
+        global scale
+        if scale != 1:
+            scale -= 1
+            self.getImage()
+            self.pixmap = QPixmap(self.map_file)
+            self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
